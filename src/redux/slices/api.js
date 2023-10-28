ffe24fd12704c0509ff4b17d6b8441de33178b74
Arfaoui-ghaith <ghaith.arfaoui34@gmail.com>
2023-10-28 13:43:05 +0100

    push

--- src/redux/slices/api.js
+++ src/redux/slices/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery(
     {
-        baseUrl: 'https://looky-server.onrender.com/api/v1'
+        baseUrl: 'https://looky-z1j2.onrender.com/api/v1'
     }
 );
 
